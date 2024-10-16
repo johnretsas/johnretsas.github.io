1f5ec7910f3f54e6729ea15160a6df27004aa85e
John Retsas <joretsas@gmail.com>
2024-10-16 22:13:00 +0100

    fix: workflows

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-  assetPrefix = `/${repo}/`;
-  basePath = `${repo}`;
-}
-
 module.exports = {
   assetPrefix: assetPrefix,
   basePath: "https://johnretsas.github.io/",
