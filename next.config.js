c5b29f7929e23904bee2a2e58088d9a9d3132369
johnretsas <joretsas@gmail.com>
2024-10-26 19:53:17 +0100

    fix: config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 
-const isProd = process.env.NODE_ENV !== "development";
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   assetPrefix: isProd ? "https://johnretsas.github.io" : "",
   images: {
     unoptimized: true,
   },
-  trailingSlash: true, // This is important for static exports
+  trailingSlash: true, // Important for static exports
   output: "export",
 };
